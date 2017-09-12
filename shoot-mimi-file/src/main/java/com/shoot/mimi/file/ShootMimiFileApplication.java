725af70294961568e2a9e28954145fc3a5a6b5af
mouxiao <272175223@qq.com>
2017-09-12 15:33:02 +0800

    shoot-mimi-file
    shoot-mimi-file

---
+++ shoot-mimi-file/src/main/java/com/shoot/mimi/file/ShootMimiFileApplication.java
@@ -0,0 +1,12 @@
+package com.shoot.mimi.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootMimiFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShootMimiFileApplication.class, args);
+	}
+}
