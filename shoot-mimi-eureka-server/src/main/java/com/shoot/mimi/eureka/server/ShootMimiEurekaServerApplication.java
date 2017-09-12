1a078aa93322fa4066185288fd08c82699894c6a
mouxiao <mouxiao@gmail.com>
2017-09-12 15:10:38 +0800

    增加服务注册中心相关配置

--- shoot-mimi-eureka-server/src/main/java/com/shoot/mimi/eureka/server/ShootMimiEurekaServerApplication.java
+++ shoot-mimi-eureka-server/src/main/java/com/shoot/mimi/eureka/server/ShootMimiEurekaServerApplication.java
@@ -2,8 +2,15 @@ package com.shoot.mimi.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 服务注册中心
+ * @author mouxiao(272175223@qq.com)
+ *
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class ShootMimiEurekaServerApplication {
 
 	public static void main(String[] args) {
