4cf060b38ce88d75093b772a9812ac1a3dd5eafb
mouxiao <272175223@qq.com>
2017-09-12 14:26:31 +0800

    shoot-mimi-eureka-server
    shoot-mimi-eureka-server

---
+++ shoot-mimi-eureka-server/src/main/java/com/shoot/mimi/eureka/server/ShootMimiEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.shoot.mimi.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootMimiEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShootMimiEurekaServerApplication.class, args);
+	}
+}
