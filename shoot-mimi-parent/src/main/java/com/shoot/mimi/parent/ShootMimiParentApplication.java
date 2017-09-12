b93af0e04a16b55fd526c31fe1d9ae05a26dbd0d
mouxiao <272175223@qq.com>
2017-09-12 14:21:08 +0800

    shoot-mimi-parent
    shoot-mimi-parent

---
+++ shoot-mimi-parent/src/main/java/com/shoot/mimi/parent/ShootMimiParentApplication.java
@@ -0,0 +1,12 @@
+package com.shoot.mimi.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShootMimiParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShootMimiParentApplication.class, args);
+	}
+}
---
+++ shoot-mimi-parent/src/test/java/com/shoot/mimi/parent/ShootMimiParentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shoot.mimi.parent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShootMimiParentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
