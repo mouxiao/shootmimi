036ef309d9d5e4844e89a36565b5991c62770217
mouxiao <272175223@qq.com>
2017-09-14 10:37:57 +0800

    portal
    新增portal启动类

---
+++ shoot-mimi-portal/src/test/java/com/shoot/mimi/portal/ShootMimiPortalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shoot.mimi.portal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShootMimiPortalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
