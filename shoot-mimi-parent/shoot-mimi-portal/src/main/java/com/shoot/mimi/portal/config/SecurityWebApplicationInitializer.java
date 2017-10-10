88b6d0a94d5c4ce9a9db9466235b18e578c85c68
tianyi <tianyi@gshopper.com>
2017-10-10 10:30:50 +0800

    集成spring security及cas单点登录

---
+++ shoot-mimi-parent/shoot-mimi-portal/src/main/java/com/shoot/mimi/portal/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.shoot.mimi.portal.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by luoxx on 2017/10/4.
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ shoot-mimi-parent/shoot-mimi-portal/src/main/java/com/shoot/mimi/portal/controller/IndexController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C), 2017-2017, 帮5采
+ * FileName: indexController
+ * Author:   tianyi
+ * Date:     2017/10/10 9:55
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.shoot.mimi.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author tianyi
+ * @create 2017/10/10
+ * @since 1.0.0
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+
+}
