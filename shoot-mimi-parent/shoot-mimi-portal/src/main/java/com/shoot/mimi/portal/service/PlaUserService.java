b36539cfff04d458b3fca148712b2f528c1c2a1a
mouxiao <mouxiao1014@gmail.com>
2017-10-24 12:01:14 +0800

    测试接口

--- shoot-mimi-parent/shoot-mimi-portal/src/main/java/com/shoot/mimi/portal/service/PlaUserService.java
+++ shoot-mimi-parent/shoot-mimi-portal/src/main/java/com/shoot/mimi/portal/service/PlaUserService.java
@@ -1,14 +1,10 @@
 package com.shoot.mimi.portal.service;
 
-import java.util.Map;
-
-import org.mybatis.spring.annotation.MapperScan;
-
 import com.shoot.mimi.portal.entity.PlaUser;
 
 public interface PlaUserService {
 	
-	public PlaUser getUserByUserNameAndPassword(Map<String, Object> map);
+	public PlaUser getUserByUserNameAndPassword(String userName);
 	
 	
 }
--- shoot-mimi-parent/shoot-mimi-portal/src/main/java/com/shoot/mimi/portal/service/impl/PlaUserServiceImpl.java
+++ shoot-mimi-parent/shoot-mimi-portal/src/main/java/com/shoot/mimi/portal/service/impl/PlaUserServiceImpl.java
@@ -2,14 +2,22 @@ package com.shoot.mimi.portal.service.impl;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.shoot.mimi.portal.entity.PlaUser;
+import com.shoot.mimi.portal.mapper.PlaUserMapper;
 import com.shoot.mimi.portal.service.PlaUserService;
 
+@Service("plaUserService")
 public class PlaUserServiceImpl implements PlaUserService{
 
+	@Autowired
+	private PlaUserMapper userMapper;
+	
 	@Override
-	public PlaUser getUserByUserNameAndPassword(Map<String, Object> map) {
-		return null;
+	public PlaUser getUserByUserNameAndPassword(String userName) {
+		return userMapper.getUserOne(userName);
 	}
 
 }
