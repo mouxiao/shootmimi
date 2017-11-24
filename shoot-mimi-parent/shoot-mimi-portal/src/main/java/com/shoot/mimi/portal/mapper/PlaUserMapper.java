71ca934bfa4aa241b65431c325fe7340294a0f6c
mouxiao <mouxiao1014@gmail.com>
2017-11-24 17:10:26 +0800

    修改密码

--- shoot-mimi-parent/shoot-mimi-portal/src/main/java/com/shoot/mimi/portal/mapper/PlaUserMapper.java
+++ shoot-mimi-parent/shoot-mimi-portal/src/main/java/com/shoot/mimi/portal/mapper/PlaUserMapper.java
@@ -1,12 +1,12 @@
 package com.shoot.mimi.portal.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.mybatis.spring.annotation.MapperScan;
 
 import com.shoot.mimi.portal.entity.PlaUser;
 
-@MapperScan
+@Mapper
 public interface PlaUserMapper {
 	
 	@Select("select * from PLA_USER_ where USER_NAME_ = #{userName}")
